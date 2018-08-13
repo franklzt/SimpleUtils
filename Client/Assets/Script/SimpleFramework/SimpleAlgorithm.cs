0896de2c7b37962b27427c90c4015c4f0e9ab1bf
franklzt <289663346@qq.com>
2018-08-13 22:38:37 +0800

    Add getData by other type which in generic

--- Client/Assets/Script/SimpleFramework/SimpleAlgorithm.cs
+++ Client/Assets/Script/SimpleFramework/SimpleAlgorithm.cs
@@ -2,7 +2,7 @@
 
 namespace SimpleFramework
 {
-    public  class SimpleBase
+    public class SimpleBase
     {
         public int CompareID { get; }
         public bool TheSameAs(SimpleBase other)
@@ -24,13 +24,23 @@ namespace SimpleFramework
         {
             for (int i = 0; i < listData.Count; i++)
             {
-                if(listData[i].TData.TheSameAs(other.TData))
+                if (listData[i].TData.TheSameAs(other.TData))
                 {
                     listData[i] = other;
                     break;
-                }              
+                }
             }
             listData.Add(other);
+        }
+
+        public SimpleStruct<SimpleBase, U> OtherTypeData<V>(SimpleStruct<SimpleBase, V> OtherType)
+        {
+            return GetItemData(OtherType.TData.CompareID);
+        }
+
+        public U GetDataByID(int compareID)
+        {
+            return GetItemData(compareID).UData;
         }
 
         public SimpleStruct<SimpleBase, U> GetItemData(int CompareID)
@@ -60,19 +70,28 @@ namespace SimpleFramework
         }
     }
 
- 
-    public class SimpleModelManager : SimpleGeneric<SimpleStruct<SimpleBase, SimpleModel>, SimpleModel> 
+    public class SimpleOtherModel
+    {
+        public string OtherData { get; }
+        public SimpleOtherModel(string otheData)
+        {
+            OtherData = otheData;
+        }
+    }
+
+
+    public class SimpleModelManager : SimpleGeneric<SimpleStruct<SimpleBase, SimpleModel>, SimpleModel>
     {
         public SimpleModelManager()
         {
-            
+
         }
 
     }
 
-    public  static class StructHelp
+    public static class StructHelp
     {
-        public static SimpleStruct<SimpleBase, U> Construct<U>(this object obj, int CompareId,U u)
+        public static SimpleStruct<SimpleBase, U> Construct<U>(this object obj, int CompareId, U u)
         {
             return new SimpleStruct<SimpleBase, U>(new SimpleBase(CompareId), u);
         }
@@ -80,11 +99,12 @@ namespace SimpleFramework
 
     public class Test
     {
-        public  void AssertTest()
+        public void AssertTest()
         {
             SimpleModelManager simpleModelManager = new SimpleModelManager();
             simpleModelManager.AddOrUpdate(this.Construct(1, new SimpleModel("SimpleModelData")));
+            simpleModelManager.OtherTypeData(this.Construct(1, new SimpleOtherModel("OtherData")));
         }
     }
-        
+
 }
