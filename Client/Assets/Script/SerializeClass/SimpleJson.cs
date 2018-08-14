4fd456ead325ea42a0b66139e55f164db6aeecaf
franklzt <289663346@qq.com>
2018-08-14 19:30:25 +0800

    Add json plugins

---
+++ Client/Assets/Script/SerializeClass/SimpleJson.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+
+public class DogItem
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public string name { get; set; }
+    /// <summary>
+    /// 
+    /// </summary>
+    public string breed { get; set; }
+    /// <summary>
+    /// 
+    /// </summary>
+    public int count { get; set; }
+    /// <summary>
+    /// 
+    /// </summary>
+    public string twoFeet { get; set; }
+}
+
+public class Cat
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public string name { get; set; }
+}
+
+public class Animals
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public List<DogItem> dog { get; set; }
+    /// <summary>
+    /// 
+    /// </summary>
+    public Cat cat { get; set; }
+}
+
+public class AnimalsJsonRoot
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public Animals animals { get; set; }
+}
---
+++ Client/Assets/Script/SerializeJson/SerializeMonoTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Newtonsoft.Json;
+
+public class SerializeMonoTest : MonoBehaviour
+{
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+        string jsonText = Resources.Load<TextAsset>("json/jsondemo").text;
+        Debug.Log(jsonText);
+        AnimalsJsonRoot jsonRoot = JsonConvert.DeserializeObject<AnimalsJsonRoot>(jsonText);
+        Debug.Log(jsonRoot);
+
+    }
+
+
+}
